a655b007fb9b580a2e8453e64a53c245b9986185
farruxxamidov <farruxxamidov@gmail.com>
2020-03-16 15:12:47 +0500

    feat: add "Singolo. DOM" Slider. Переключение слайдов; Slider. Активация экранов телефонов

--- script.js
+++ script.js
@@ -11,6 +11,70 @@ window.onload = function () {
     addPicturesClickHandler();
 }
 
+/* SLIDER */
+
+const sliderBackground = document.querySelector(".slider");
+const slideContainer = document.querySelector(".slider__images");
+const arrow = document.querySelectorAll(".slider .arrow");
+
+const slides = {
+  0: [`<div class="iphone iphone_vertical">
+        <div class="iphone__screen"></div>
+        <div class="iphone__phone"></div>
+        <div class="iphone__shadow"></div>
+       </div>`,
+      `<div class="iphone iphone_horizontal">
+        <div class="iphone__screen"></div>
+        <div class="iphone__phone"></div>
+        <div class="iphone__shadow"></div>
+       </div>`],
+  1: [`<img width="517" height="513" src="./assets/img/slider-images/2-iphones.png" alt="iPhone Vertical">`]
+};
+
+const slideColor = {
+  0: "bg-red",
+  1: "bg-blue",
+  "default": "bg-red"
+}
+
+// Phone screens switching off / on
+function phoneScreensActivate() {
+  const phones = document.querySelectorAll(".slider .iphone__screen");
+  phones.forEach( phone => phone.addEventListener( "click", event => {
+    if ( event.target.classList.contains("hidden") ) {
+      event.target.classList.remove("hidden");
+    } else {
+      event.target.classList.add("hidden");
+    }
+  }));
+}
+phoneScreensActivate();
+
+let currentSlide = 0;
+arrow.forEach( each => each.addEventListener("click", event => {
+  ( event.target.classList.contains("left") ) ? currentSlide-- : currentSlide++;
+
+  if (currentSlide == Object.keys(slides).length) {
+    currentSlide = 0;
+  } else if (currentSlide < 0) {
+    currentSlide = Object.keys(slides).length-1;
+  }
+
+  sliderBackground.classList.value = "slider home";
+
+  if(slideColor.hasOwnProperty(currentSlide)) {
+    sliderBackground.classList.add(slideColor[currentSlide]);
+  } else {
+    sliderBackground.classList.add( slideColor["default"] );
+  }
+
+  slideContainer.innerHTML = "";
+  slides[currentSlide].forEach( img => slideContainer.innerHTML += `\n${img}`);
+
+  phoneScreensActivate();
+}));
+
+
 //Menu function expressions
 const addMenuClickHandler = () => {
     const MENU = document.getElementById('menu');
