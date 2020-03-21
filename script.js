7d5f4c0a3d594fb2ac367b4f3bfc3edf7eaf3fde
farruxxamidov <farruxxamidov@gmail.com>
2020-03-21 19:12:05 +0500

    feat: add "Singolo. DOM" Get a quote

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-window.onload = function () {
-    console.log('Hello Rolling Scopes!');
+/* MENU */
 
-    //Menu
-    addMenuClickHandler();
+const menu = document.querySelector("nav ul");
+const menuLinks = document.querySelectorAll("nav ul li a");
 
-    //Tags
-    addTagsClickHandler();
+menuLinks.forEach( link => link.addEventListener("click", (event) => {
+  menu.querySelectorAll('a').forEach(e => e.classList.remove('current'));
+  event.target.classList.add("current");
+}));
 
-    //Pictures
-    addPicturesClickHandler();
+document.addEventListener("scroll", changeMenuActiveLink);
+window.onload = changeMenuActiveLink();
+
+function changeMenuActiveLink(event) {
+  const currentPositionY = window.scrollY;
+  const tagsWithId = document.querySelectorAll('[id]');
+
+  tagsWithId.forEach( tag => {
+    if (tag.offsetTop - 89 <= currentPositionY &&
+       (tag.offsetTop + tag.offsetHeight - 89) > currentPositionY) {
+      menuLinks.forEach( link => {
+        link.classList.remove("current");
+        if(tag.getAttribute("id") === link.getAttribute("href").substring(1)) {
+          link.classList.add("current");
+        }
+      });
+    }
+  });
+  
 }
+
+
 
 /* SLIDER */
 
-const sliderBackground = document.querySelector(".slider");
-const slideContainer = document.querySelector(".slider__images");
-const arrow = document.querySelectorAll(".slider .arrow");
-
 const slides = {
-  0: [`<div class="iphone iphone_vertical">
-        <div class="iphone__screen"></div>
-        <div class="iphone__phone"></div>
+  0: `<div class="iphone iphone_vertical">
+        <div class="iphone__screen iphone__clickable"></div>
+        <div class="iphone__phone iphone__clickable"></div>
+        <div class="iphone__shadow"></div>
+       </div>
+      <div class="iphone iphone_horizontal">
+        <div class="iphone__screen iphone__clickable"></div>
+        <div class="iphone__phone iphone__clickable"></div>
         <div class="iphone__shadow"></div>
        </div>`,
-      `<div class="iphone iphone_horizontal">
-        <div class="iphone__screen"></div>
-        <div class="iphone__phone"></div>
-        <div class="iphone__shadow"></div>
-       </div>`],
-  1: [`<img width="517" height="513" src="./assets/img/slider-images/2-iphones.png" alt="iPhone Vertical">`]
+  1: `<img width="517" height="513" src="./assets/slider-images/2-iphones.png" alt="iPhone Vertical">`
 };
 
 const slideColor = {
@@ -37,102 +53,172 @@ const slideColor = {
   "default": "bg-red"
 }
 
+const sliderBackground = document.querySelector(".slider");
+let slidesContainer = document.querySelector(".slider__images");
+const arrow = document.querySelectorAll(".slider .arrow");
+let currentSlide = 0;
+let slidesNumber = Object.keys(slides).length;
+
+// Init slider
+initSlider(currentSlide);
+function initSlider(currentSlide) {
+  slidesContainer.innerHTML = "";
+  for (let i = 0; i < slidesNumber; i++) {
+    let slide = document.createElement("div");
+    slide.innerHTML = slides[i];
+    slide.classList = "slide";
+    if (i == currentSlide) slide.classList.add("active");
+    slidesContainer.append(slide);
+  }
+  phoneScreensActivate();
+}
+
 // Phone screens switching off / on
 function phoneScreensActivate() {
-  const phones = document.querySelectorAll(".slider .iphone__screen");
-  phones.forEach( phone => phone.addEventListener( "click", event => {
-    if ( event.target.classList.contains("hidden") ) {
-      event.target.classList.remove("hidden");
-    } else {
-      event.target.classList.add("hidden");
-    }
-  }));
+  const phones = document.querySelectorAll(".slider .iphone");
+  phones.forEach( phone => phone.querySelectorAll(".iphone__clickable").forEach( 
+    element => element.addEventListener( "click", event => {
+      let screen = phone.querySelector(".iphone__screen");
+      (screen.classList.contains("hidden")) ?
+        screen.classList.remove("hidden") :
+        screen.classList.add("hidden");
+    })
+  ));
 }
-phoneScreensActivate();
 
-let currentSlide = 0;
-arrow.forEach( each => each.addEventListener("click", event => {
-  ( event.target.classList.contains("left") ) ? currentSlide-- : currentSlide++;
+// Slider actions
+let slidesList = slidesContainer.querySelectorAll(".slide");
 
-  if (currentSlide == Object.keys(slides).length) {
-    currentSlide = 0;
-  } else if (currentSlide < 0) {
-    currentSlide = Object.keys(slides).length-1;
-  }
+function hideSlide(direction) {
+  slidesList[currentSlide].classList.add(direction);
+  slidesList[currentSlide].addEventListener("animationend", function(){
+    this.classList.remove("active", direction);
+  });
+}
 
-  sliderBackground.classList.value = "slider home";
+function showSlide(direction) {
+  slidesList[currentSlide].classList.add("next", direction);
+  slidesList[currentSlide].addEventListener("animationend", function(){
+    this.classList.remove("next", direction);
+    this.classList.add("active");
+  });
+}
 
+function changeSlideBackground(){
+  sliderBackground.classList.value = "slider";
   if(slideColor.hasOwnProperty(currentSlide)) {
     sliderBackground.classList.add(slideColor[currentSlide]);
   } else {
     sliderBackground.classList.add( slideColor["default"] );
   }
+}
 
-  slideContainer.innerHTML = "";
-  slides[currentSlide].forEach( img => slideContainer.innerHTML += `\n${img}`);
-
-  phoneScreensActivate();
+arrow.forEach( each => each.addEventListener("click", event => {
+  if ( event.target.classList.contains("right") ) {
+    hideSlide("to-right");
+    currentSlide = (--currentSlide + slidesNumber) % slidesNumber;
+    changeSlideBackground();
+    showSlide("from-left");
+  } else {
+    hideSlide("to-left");
+    currentSlide = (++currentSlide + slidesNumber) % slidesNumber;
+    changeSlideBackground();
+    showSlide("from-right");
+  }
 }));
 
 
-//Menu function expressions
-const addMenuClickHandler = () => {
-    const MENU = document.getElementById('menu');
 
-    MENU.addEventListener('click', (event) => {
-        MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
-        event.target.classList.add('active');
-    })
+/* PORTFOLIO */
+
+const tags = document.querySelectorAll(".portfolio__tags button");
+const portfolio = document.querySelector(".portfolio__pictures");
+
+// Shift portfolio pictures by clicking on tag
+tags.forEach(tag => tag.addEventListener("click", (event) => {
+  // Prevent selected tag from click action
+  if( !event.target.classList.contains("selected") ) {
+    let portfolioPictures = [...portfolio.querySelectorAll(".portfolio__picture")];
+    portfolioPictures.unshift(portfolioPictures.pop());
+    portfolioPictures.forEach( pic => portfolio.append(pic) );
+  }
+  tags.forEach(t => t.classList.remove('selected'));
+  event.target.classList.add("selected");
+}));
+
+// Make clicked portfolio picture bordered
+let switchNow = true;
+const portfolioPic = portfolio.querySelectorAll(".portfolio__picture");
+portfolioPic.forEach(image => image.addEventListener("click", (event) => {
+  if ( event.target.classList.contains("bordered") ) {
+    switchNow = false;
+  }
+
+  portfolio.querySelectorAll("img").forEach(pic => pic.classList.remove("bordered"));
+  
+  if (switchNow) {
+    event.target.classList.add("bordered");
+  }
+
+  switchNow = true;
+}));
+
+
+
+/* MODAL WINDOW */
+
+const button = document.querySelector("form button");
+const modal = document.querySelector(".modal");
+const modalBackground = document.querySelector(".modal__background");
+const modalMessage = document.querySelector(".modal__message");
+
+//Add Close button to modal window
+function addCloseButton(node){
+  node.innerHTML += "<button class='modal__close-button' type='button'>OK</button>";
+  const modalCloseButton = document.querySelector(".modal__close-button");
+  modalCloseButton.addEventListener("click", hideModal);
+  return node;
 }
 
-//Pictures function expressions
-const addPicturesClickHandler = () => {
-    document.querySelector('.layout-4-column').addEventListener('click', (e) => {
-        if (e.target.classList.contains('portfolio__picture')) {
-            let clickedPicture = e.target;
-            removeSelectedPictures();
-            selectClickedPicture(clickedPicture);
-        }
-    })    
+//Get value from form field
+function addNodeValue (node, defaultValue = "Не заполнено") {
+  let value = document.querySelector(node).value;
+  value = (value == "") ? defaultValue : value;
+  return value;
 }
 
-const removeSelectedPictures = () => {
-    let pictures = document.querySelectorAll('.layout-4-column .portfolio__picture');
-    pictures.forEach(picture => {
-        picture.classList.remove('picture_selected');
-    })
+//Show modal window
+function showModal () {
+  modal.classList.remove("hidden");
 }
 
-const selectClickedPicture = (clickedPicture) => {
-    clickedPicture.classList.add('picture_selected');
+//Hide modal window
+function hideModal () {
+  modal.classList.add("hidden");
+  document.forms[0].reset();
 }
 
+//Create content of modal window
+button.addEventListener("click", (event) => {
+  let requiredFields = [...document.querySelectorAll("[required]")];
+  let isValid = node => node.checkValidity();
 
-//Tags function expressions
-const addTagsClickHandler = () => {
-    document.querySelector('.portfolio__tags').addEventListener('click', (e) => {
-        if (e.target.classList.contains('tag')) {
-            let clickedTag = e.target;
-            removeSelectedTags();
-            selectClickedTag(clickedTag);
-            changePictureOrder();
-        }
-    })
-}
+  //Check if all required fields filled with valid data
+  if ( requiredFields.every(isValid) ) {
+    event.preventDefault();
 
-const removeSelectedTags = () => {
-    let tags = document.querySelectorAll('.portfolio__tags .tag');
-    tags.forEach(tag => {
-        tag.classList.remove('selected');
-    })
-}
+    modalMessage.innerHTML = "";
+    let title = document.createElement("p");
+    title.innerText = "Письмо отправлено";
+    let subject = document.createElement("p");
+    subject.innerText = "Тема: " + addNodeValue("input[name='subject']", "Без темы");
+    let description = document.createElement("p");
+    description.innerText = "Описание: " + addNodeValue("textarea[name='message']", "Без Описания");
+    modalMessage.append(title, subject, description);
+    addCloseButton(modalMessage);
+    showModal();
+  }
+});
 
-const selectClickedTag = (clickedTag) => {
-    clickedTag.classList.add('selected');
-}
-
-const changePictureOrder = () => {
-    let picturesParent = document.querySelector('.layout-4-column');
-    let pictures = document.querySelectorAll('.layout-4-column .portfolio__picture');
-    picturesParent.appendChild(pictures.item(0));
-}
+// Add close action to modal window background
+modalBackground.addEventListener("click", hideModal);
